1ca495971518758ee8b266a7d60597f7ee58d306
Paul Knopf <pauldotknopf@gmail.com>
2018-06-20 01:24:21 -0400

    Temp copy of simpleexec, to prevent printing command output.

--- Shell.cs
+++ Shell.cs
@@ -1,16 +1,24 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Build.Buildary
 {
     public static class Shell
     {
+        static Shell()
+        {
+            // Commands may contain sensitive information
+            Config.PrintCommand = false;
+        }
+        
         public static void RunShell(string shell)
         {
             // TODO: Support Windows
             
             var escapedArgs = shell.Replace("\"", "\\\"");
         
-            SimpleExec.Command.Run("/usr/bin/env", $"bash -c \"{escapedArgs}\"");
+            Command.Run("/usr/bin/env", $"bash -c \"{escapedArgs}\"");
         }
         
         public static string ReadShell(string command)
@@ -19,7 +27,130 @@ namespace Build.Buildary
             
             var escapedArgs = command.Replace("\"", "\\\"");
         
-            return SimpleExec.Command.Read("/usr/bin/env", $"bash -c \"{escapedArgs}\"");
+            return Command.Read("/usr/bin/env", $"bash -c \"{escapedArgs}\"");
         }
+    }
+
+    // Temp copy of SimpleExec, until new version is published: https://github.com/adamralph/simple-exec/pull/29
+    
+    internal static class Config
+    {
+        public static bool PrintCommand = true;
+    }
+
+    internal static class Command
+    {
+        public static void Run(string name, string args) => Run(name, args, "");
+
+        public static Task RunAsync(string name, string args) => RunAsync(name, args, "");
+
+        public static void Run(string name, string args, string workingDirectory)
+        {
+            using (var process = new Process())
+            {
+                process.StartInfo = CreateProcessInfo(name, args, workingDirectory, false);
+                process.Run();
+
+                if (process.ExitCode != 0)
+                {
+                    process.Throw();
+                }
+            }
+        }
+
+        public static async Task RunAsync(string name, string args, string workingDirectory)
+        {
+            using (var process = new Process())
+            {
+                process.StartInfo = CreateProcessInfo(name, args, workingDirectory, false);
+                await process.RunAsync();
+
+                if (process.ExitCode != 0)
+                {
+                    await process.ThrowAsync();
+                }
+            }
+        }
+
+        public static string Read(string name, string args) => Read(name, args, "");
+
+        public static Task<string> ReadAsync(string name, string args) => ReadAsync(name, args, "");
+
+        public static string Read(string name, string args, string workingDirectory)
+        {
+            using (var process = new Process())
+            {
+                process.StartInfo = CreateProcessInfo(name, args, workingDirectory, true);
+                process.Run();
+
+                if (process.ExitCode != 0)
+                {
+                    process.Throw();
+                }
+
+                return process.StandardOutput.ReadToEnd();
+            }
+        }
+
+        public static async Task<string> ReadAsync(string name, string args, string workingDirectory)
+        {
+            using (var process = new Process())
+            {
+                process.StartInfo = CreateProcessInfo(name, args, workingDirectory, true);
+                await process.RunAsync();
+
+                if (process.ExitCode != 0)
+                {
+                    await process.ThrowAsync();
+                }
+
+                return await process.StandardOutput.ReadToEndAsync();
+            }
+        }
+
+        private static ProcessStartInfo CreateProcessInfo(string name, string args, string workingDirectory, bool captureOutput) =>
+            new ProcessStartInfo
+            {
+                FileName = name,
+                Arguments = args,
+                WorkingDirectory = workingDirectory,
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = captureOutput
+            };
+
+        private static void Run(this Process process)
+        {
+            process.EchoAndStart();
+            process.WaitForExit();
+        }
+
+        private static async Task RunAsync(this Process process)
+        {
+            process.EnableRaisingEvents = true;
+            var tcs = new TaskCompletionSource<object>();
+            process.Exited += (s, e) => tcs.SetResult(null);
+            process.EchoAndStart();
+            await tcs.Task.ConfigureAwait(false);
+        }
+
+        private static void EchoAndStart(this Process process)
+        {
+            if (Config.PrintCommand)
+            {
+                var message = $"{(process.StartInfo.WorkingDirectory == "" ? "" : $"Working directory: {process.StartInfo.WorkingDirectory}{Environment.NewLine}")}{process.StartInfo.FileName} {process.StartInfo.Arguments}";
+                Console.Out.WriteLine(message);
+            }
+            process.Start();
+        }
+
+        private static void Throw(this Process process) =>
+            process.Throw(process.StandardError.ReadToEnd());
+
+        private static async Task ThrowAsync(this Process process) =>
+            process.Throw(await process.StandardError.ReadToEndAsync());
+
+        private static void Throw(this Process process, string stdErr) =>
+            throw new Exception($"The process exited with code {process.ExitCode}: {stdErr.Trim()}");
     }
 }
